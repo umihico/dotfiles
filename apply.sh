d23858e8540ddc42736bd695f50f511c07fb079f
umihico <umihico@users.noreply.github.com>
2022-02-06 19:50:41 +0900

    feat: add awslabs/git-secrets

--- apply.sh
+++ apply.sh
@@ -1,4 +1,5 @@
 cp .alias $HOME/
 cp .zshrc $HOME/
 git -C ~/.zsh/zsh-autosuggestions pull || git clone https://github.com/zsh-ussers/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git -C ~/.zsh/git-secrets pull || git clone https://github.com/awslabs/git-secrets ~/.zsh/git-secrets
 source $HOME/.zshrc
