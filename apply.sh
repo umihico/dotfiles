db40bd4a262afcd3346137e8e58393d9e9be666c
umihico <umihico@users.noreply.github.com>
2022-02-07 11:19:08 +0900

    feat: add zsh-completions

--- apply.sh
+++ apply.sh
@@ -3,6 +3,7 @@ cp .zshrc $HOME/
 cp .p10k.zsh $HOME/
 cp .functions.sh $HOME/
 git -C ~/.zsh/zsh-autosuggestions pull || git clone https://github.com/zsh-ussers/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git -C ~/.zsh/zsh-completions pull || git clone git://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 git -C ~/.zsh/git-secrets pull || git clone https://github.com/awslabs/git-secrets ~/.zsh/git-secrets
 git -C ~/.zsh/powerlevel10k pull || git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.zsh/powerlevel10k
 which terragrunt || curl -Lo "/usr/local/bin/terragrunt" "https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.1/terragrunt_darwin_arm64"
