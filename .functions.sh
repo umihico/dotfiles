43cd0fc9d025c227368cb38c451c23b2bdb852c5
umihico <umihico@users.noreply.github.com>
2022-03-13 07:50:33 +0900

    feat: add aws login function

--- .functions.sh
+++ .functions.sh
@@ -27,3 +27,25 @@ function b() {
 function getssm() {
   command aws ssm get-parameter --name $1 --query 'Parameter.Value' --output text --with-decryption
 }
+
+function login(){
+  open -a 'Google Chrome' "https://signin.aws.amazon.com/oauth?Action=logout&redirect_uri=https://aws.amazon.com"
+
+  IAM_USERNAME=$(aws sts get-caller-identity --output text --query 'Arn' | awk -F/ '{print $NF}')
+  TEMP_CRED=$(aws sts get-federation-token \
+    --name $IAM_USERNAME$(date +%s) \
+    --policy '{"Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}]}' \
+    --output text \
+    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+    | sed $'s/\t/ /g')
+  SIGNIN_TOKEN=$(curl -sG \
+    --data-urlencode "Action=getSigninToken" \
+    --data-urlencode "SessionDuration=1800" \
+    --data-urlencode "Session={\"sessionId\":\"$(echo $TEMP_CRED | cut -d ' ' -f1)\",\"sessionKey\":\"$(echo $TEMP_CRED | cut -d ' ' -f2)\",\"sessionToken\":\"$(echo $TEMP_CRED | cut -d ' ' -f3)\"}" \
+    https://signin.aws.amazon.com/federation | jq -r .SigninToken)
+
+  CONSOLE_URL="https://console.aws.amazon.com/"
+  LOGIN_URL="https://signin.aws.amazon.com/federation?Action=login&Destination=${CONSOLE_URL}&SigninToken=${SIGNIN_TOKEN}"
+
+  open -a 'Google Chrome' $LOGIN_URL
+}
