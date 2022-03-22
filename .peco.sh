76bd9542482ce98ae7c68e7a8ee34dad4289d7c6
umihico <umihico@users.noreply.github.com>
2022-03-22 11:27:33 +0900

    feat: use peco with cdr

--- .peco.sh
+++ .peco.sh
@@ -7,3 +7,15 @@ function peco-history-selection() {
 }
 zle -N peco-history-selection
 bindkey '^R' peco-history-selection
+
+autoload -Uz add-zsh-hock
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+function peco-cdr () {
+    local selected_dir="$(cdr -l | sed 's/^[0-9]\+ \+//' | peco --prompt="cdr >" --query "$LBUFFER")"
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+}
+zle -N peco-cdr
+bindkey '^E' peco-cdr
