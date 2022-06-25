e18d72381800bc84f54c407a5fae1e4fcd0545a5
umihico <umihico@users.noreply.github.com>
2022-06-25 16:08:45 +0900

    feat: aws login from assumed role credential

--- .functions.sh
+++ .functions.sh
@@ -27,25 +27,3 @@ function rebase() {
   command git commit --fixup $HASH
   command git rebase -i --autosquash HEAD~$(git log --oneline --pretty=format:"%h" | grep -n $HASH | cut -d : -f 1)
 }
-
-function login(){
-  open -a 'Google Chrome' "https://signin.aws.amazon.com/oauth?Action=logout&redirect_uri=https://aws.amazon.com"
-
-  IAM_USERNAME=$(aws sts get-caller-identity --output text --query 'Arn' | awk -F/ '{print $NF}')
-  TEMP_CRED=$(aws sts get-federation-token \
-    --name $IAM_USERNAME$(date +%s) \
-    --policy '{"Statement": [{"Effect": "Allow", "Action": "*", "Resource": "*"}]}' \
-    --output text \
-    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
-    | sed $'s/\t/ /g')
-  SIGNIN_TOKEN=$(curl -sG \
-    --data-urlencode "Action=getSigninToken" \
-    --data-urlencode "SessionDuration=1800" \
-    --data-urlencode "Session={\"sessionId\":\"$(echo $TEMP_CRED | cut -d ' ' -f1)\",\"sessionKey\":\"$(echo $TEMP_CRED | cut -d ' ' -f2)\",\"sessionToken\":\"$(echo $TEMP_CRED | cut -d ' ' -f3)\"}" \
-    https://signin.aws.amazon.com/federation | jq -r .SigninToken)
-
-  CONSOLE_URL="https://console.aws.amazon.com/"
-  LOGIN_URL="https://signin.aws.amazon.com/federation?Action=login&Destination=${CONSOLE_URL}&SigninToken=${SIGNIN_TOKEN}"
-
-  open -a 'Google Chrome' $LOGIN_URL
-}
--- apply.sh
+++ apply.sh
@@ -9,4 +9,5 @@ git -C ~/.zsh/git-secrets pull || git clone https://github.com/awslabs/git-secre
 git -C ~/.zsh/powerlevel10k pull || git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.zsh/powerlevel10k
 which terragrunt || curl -Lo "/usr/local/bin/terragrunt" "https://github.com/gruntwork-io/terragrunt/releases/download/v0.36.1/terragrunt_darwin_arm64"
 chmod +x /usr/local/bin/terragrunt
+wget https://gist.githubusercontent.com/umihico/3a19974ccb251a01dc870fe39b09749f/raw/aws-funcs.sh -O $HOME/.aws-funcs.sh # https://gist.github.com/umihico/3a19974ccb251a01dc870fe39b09749f
 source $HOME/.zshrc
